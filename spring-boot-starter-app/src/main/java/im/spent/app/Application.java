2a590d71e2b90d835fde98f4b46cb54b5eef3d37
zhangxishuo <zhangxishuo1998@outlook.com>
2020-12-18 00:49:37 +0800

    auto-configure

--- spring-boot-starter-app/src/main/java/im/spent/app/Application.java
+++ spring-boot-starter-app/src/main/java/im/spent/app/Application.java
@@ -1,12 +1,12 @@
-package im.spent.demo;
+package im.spent.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StarterApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(StarterApplication.class);
+        SpringApplication.run(Application.class);
     }
 }
--- spring-boot-starter-mybatis/src/main/java/im/spent/mybatis/conf/MybatisAutoConfiguration.java
+++ spring-boot-starter-mybatis/src/main/java/im/spent/mybatis/conf/MybatisAutoConfiguration.java
@@ -1,18 +1,19 @@
 package im.spent.mybatis.conf;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import tk.mybatis.mapper.autoconfigure.MybatisProperties;
 
 @Configuration
-@EnableAutoConfiguration
 public class MybatisAutoConfiguration {
 
-    @Bean
-    @ConditionalOnMissingBean
+    private static final Logger logger = LoggerFactory.getLogger(MybatisAutoConfiguration.class);
+
+    @Bean("mybatis-tk.mybatis.mapper.autoconfigure.MybatisProperties")
     public MybatisProperties mybatisProperties() {
+        logger.info("Auto Configure MyBatis Properties");
         org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
         configuration.setMapUnderscoreToCamelCase(true);
 
