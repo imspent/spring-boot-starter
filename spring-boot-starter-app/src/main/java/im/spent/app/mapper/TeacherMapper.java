409caff1292c236bfe8b2eb576f2afca42e6ff6a
zhangxishuo <zhangxishuo1998@outlook.com>
2020-12-24 10:06:21 +0800

    mapper

--- spring-boot-starter-app/src/main/java/im/spent/app/mapper/TeacherMapper.java
+++ spring-boot-starter-app/src/main/java/im/spent/app/mapper/TeacherMapper.java
@@ -1,10 +1,13 @@
 package im.spent.app.mapper;
 
 import im.spent.app.entity.Teacher;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface TeacherMapper {
 
+    @Insert("INSERT INTO teacher(id, name, username, email, sex, create_time, update_time)\n" +
+            "VALUES(#{id}, #{name}, #{username}, #{email}, #{sex}, #{createTime}, #{updateTime})")
     void insert(Teacher teacher);
 }
